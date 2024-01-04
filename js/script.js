f7ae1068627b682a15b890e92ea2be5c32eed516
Katarzyna Bogusz <kabogusz@interia.pl>
2024-01-04 22:53:27 +0100

    Add changes in js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,9 @@
-const buttonTest = document.getElementById('button-test');
 const buttonRock = document.getElementById('button-rock');
 const buttonPaper = document.getElementById('button-paper');
 const buttonScissors = document.getElementById('button-scissors');
 function buttonClicked(argButtonName) {
   clearMessages();
   console.log(argButtonName + ' został kliknięty');
-  let argComputerMove,
-    argMoveId,
-    argPlayerMove,
-    computerMove,
-    playerInput,
-    playerMove,
-    randomNumber;
 
   function getMoveName(argMoveId) {
     console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
@@ -50,12 +42,11 @@ function buttonClicked(argButtonName) {
     }
     printMessage('I played ' + argComputerMove + ', and You ' + argPlayerMove);
   }
-  playerMove = argButtonName;
-  console.log('ruch gracza to: ' + playerInput);
+  const playerMove = argButtonName;
   console.log('ruch gracza to: ' + playerMove);
-  randomNumber = Math.floor(Math.random() * 3 + 1);
+  const randomNumber = Math.floor(Math.random() * 3 + 1);
   console.log('wylosowana liczba to: ' + randomNumber);
-  computerMove = getMoveName(randomNumber);
+  const computerMove = getMoveName(randomNumber);
   console.log('ruch komputera to: ' + computerMove);
   displayResult(playerMove, computerMove);
 }
